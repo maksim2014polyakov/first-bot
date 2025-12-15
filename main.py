83456e59e39fe28b3adb07fd6afa7387bc45d2aa
Максим <maxpolyakoff2014@gmail.com>
2025-12-15 17:56:10 +0300

    первый бот

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print("Hello World")
-print("Hello World")
+from aiogram import Bot, types, Dispatcher
+from aiogram.filters import Command
+from dotenv import load_dotenv
+import os, asyncio
+
+load_dotenv()
+BOT_TOKEN=os.getenv("BOT_TOKEN")
+
+bot=Bot(BOT_TOKEN)
+dp=Dispatcher()
+
+@dp.message(Command("start"))
+async def start(msg: types.Message):
+    print(msg.from_user.id, msg.from_user.full_name)
+    await msg.answer("привет я бот")
+
+@dp.message(Command("about"))
+async def about(msg: types.Message):
+    await msg.answer("этот бот может помочь тебе с учебой в школе")
+
+async def main():
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
